f574494fe1de0d788f12c7ca77891d51a3f935a4
Romain Faust <romain.faust@gmail.com>
2018-12-02 22:20:21 +0100

    refactor(api): refactor API structure to simplify its usage

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,25 +1,96 @@
 import axios from 'axios';
 
-import { refreshAccessToken } from '@api/modules/auth';
+import * as apiErrors from './errors';
+import { Module } from './types';
 
-export const authClient = axios.create({
-	baseURL: 'https://woodbox.io:3001/auth',
-	withCredentials: true,
-});
-export const cloudClient = axios.create({
-	baseURL: 'https://woodbox.io:1234/graphql',
+export const dataClient = axios.create({
+	baseURL: 'http://192.168.4.1:8080/graphql',
 });
 
-cloudClient.interceptors.response.use(undefined, async (error) => {
-	const { config, response: { status } } = error;
+function buildFetchModuleQuery(id: string, offset: number, limit: number) {
+	return `{
+		module: getModuleById(id: "${id}") {
+			id
+			name
+			type
+			location
+			mac
+			vendor
+			samples(limit: ${limit}, offset: ${offset}) {
+				date
+				payload
+			}
+		}
+	}`;
+}
 
-	if (status === 401) {
-		await refreshAccessToken();
-		await cloudClient.request(config);
-	} else {
-		throw error;
+function buildFetchModulesQuery() {
+	return `{
+		modules: allModules {
+			id
+			name
+			type
+			location
+			mac
+			vendor
+		}
+	}`;
+}
+
+function buildUpdateModuleQuery(module: Module) {
+	const { id, name, location } = module;
+	const convertedLocation = convertModuleLocation(location);
+
+	return `mutation {
+		updateModule(id: "${id}", name: "${name}", location: "${convertedLocation}") {
+			id
+		}
+	}`;
+}
+
+function convertModuleLocation(location: string) {
+	switch (location) {
+		case 'Salon':
+			return '1';
+		case 'Chambre':
+			return '2';
+		case 'Cuisine':
+			return '3';
+		default:
+			return '4';
 	}
-});
+}
 
-export * from '@api/modules/auth';
-export * from '@api/modules/modules';
+export async function fetchModule(id: string, offset: number = 0, limit: number = 10): Promise<Module> {
+	try {
+		const { data: queryData } = await dataClient.post('/', {
+			query: buildFetchModuleQuery(id, offset, limit),
+		});
+		return queryData.data.module;
+	} catch (error) {
+		throw apiErrors.UNKNOWN_ERROR;
+	}
+}
+
+export async function fetchModules(): Promise<Module[]> {
+	try {
+		const { data: queryData } = await dataClient.post('/', {
+			query: buildFetchModulesQuery(),
+		});
+		return queryData.data.modules.map((module: Partial<Module>) => {
+			return { ...module, samples: [] };
+		});
+	} catch (error) {
+		throw apiErrors.UNKNOWN_ERROR;
+	}
+}
+
+export async function updateModule(module: Module) {
+	try {
+		await dataClient.post('/', {
+			query: buildUpdateModuleQuery(module),
+		});
+	} catch (error) {
+		throw apiErrors.UNKNOWN_ERROR;
+	}
+}
